fcc4b9d0b396a0d76092d6cec0d3ff818764e098
Yves-Tabor <walvesoutis@gmail.com>
2026-08-27 23:16:10 +0200

    feat: adding backend structure

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
+import { User, UserSchema } from './user.schema';
 
 @Module({
-  providers: [UsersService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
+  providers: [UsersService],
+  exports: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule {};
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email });
+  }
+
+  async create(name: string, email: string, password: string) {
+    const user = new this.userModel({
+      name,
+      email,
+      password,
+    });
+
+    return user.save();
+  }
+}
