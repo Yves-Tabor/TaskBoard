dddda5edabafd7996fc715ee99737ffb066bee1f
Yves-Tabor <walvesoutis@gmail.com>
2026-08-28 00:36:20 +0200

    feat: adding backend structure.

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -2,7 +2,7 @@ import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 
 export class RegisterDto {
   @IsNotEmpty()
-  name: string;
+  fullName: string;
 
   @IsEmail()
   email: string;
--- backend/src/users/user.schema.ts
+++ backend/src/users/user.schema.ts
@@ -6,7 +6,7 @@ export type UserDocument = HydratedDocument<User>;
 @Schema({ timestamps: true })
 export class User {
   @Prop({ required: true })
-  name: string;
+  fullName: string;
 
   @Prop({ required: true, unique: true })
   email: string;
